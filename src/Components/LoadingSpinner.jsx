dcda8fbb8d23ba589590f8ed3146cf84f99f692a
Rafsan Rad <rafsanrad@Rafsans-MacBook-Air.local>
2025-12-05 15:30:16 +0600

    added navabar ,footer and outlet

---
+++ src/Components/LoadingSpinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingSpinner = () => {
+    return (
+        <div>
+            <h1>loading spinner</h1>
+        </div>
+    );
+};
+
+export default LoadingSpinner;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,34 @@
-import { createBrowserRouter } from "react-router";
-import App from "../App";
+import { createBrowserRouter } from "react-router"; //named import
+import Home from "../Pages/Home";
+import Products from "../Pages/Products";
+import MainLayout from "../Layouts/MainLayout";
+import ErrorPage from "../Pages/ErrorPage";
+import Wishlist from "../Pages/Wishlist";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App></App>,
+    element: <MainLayout></MainLayout>,
+    errorElement:<ErrorPage></ErrorPage>,
+    children: [
+      {
+        index:true,   // path: "/" dileo same hobe.
+        element: <Home></Home>,
+      },
+      {
+        path: "/products",
+        element: <Products></Products>,
+      },
+      {
+        path: "/wishlist",
+        element: <Wishlist></Wishlist>,
+      },
+    ],
   },
+  // eitar kaj error element kore dey.
+  // {
+  //   path:"*", //* mane define path baade onno jekono path e erropage ta show korbe.
+  //   element:<ErrorPage></ErrorPage>
+  // }
 ]);
 export default router;
